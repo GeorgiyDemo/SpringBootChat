0d7cc0b3d981941a28c8bd42be2f7888f4b6fd51
Demka <GeorgiyDemo@users.noreply.github.com>
2021-02-13 16:32:35 +0300

    Развернулись так сказат
    Теперь надо понять как тут работать

---
+++ demkaserver/src/main/java/com/demka/demkaserver/DemkaserverApplication.java
@@ -0,0 +1,13 @@
+package com.demka.demkaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemkaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemkaserverApplication.class, args);
+    }
+
+}
