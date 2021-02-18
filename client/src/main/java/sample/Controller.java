ce236ee2dfdd53480840df130f4d4b91cc7b0514
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-02-18 11:04:47 +0300

    Обработчик работает

--- client/src/main/java/sample/Controller.java
+++ client/src/main/java/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
 public class Controller {
+
+    @FXML
+    private Label AppName;
+    @FXML
+    private JFXTextField LoginTextField;
+    @FXML
+    private JFXTextField PasswordTextField;
+    @FXML
+
+    public void mainButtonClick() {
+        AppName.setText("ЛОЛ ЛОГИН ЕСТЬ");
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        //alert.initOwner(root.getPrimaryStage );
+    }
+
 }
--- client/src/main/java/sample/Main.java
+++ client/src/main/java/sample/Main.java
@@ -10,9 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 640, 400));
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/sample.fxml"));
+        Controller controller = loader.getController();
+        Parent root = loader.load();
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
