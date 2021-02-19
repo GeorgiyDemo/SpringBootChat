bf3f5fa2a82883a5b7c6f08b38faf72f2fa2ca3c
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-02-19 23:09:15 +0300

    Норм переход межу сценами

--- client/src/main/java/sample/Controller.java
+++ client/src/main/java/sample/Controller.java
@@ -2,10 +2,15 @@ package sample;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.IOException;
 
 public class Controller {
 
@@ -17,10 +22,17 @@ public class Controller {
     private JFXTextField PasswordTextField;
     @FXML
 
-    public void mainButtonClick() {
-        AppName.setText("ЛОЛ ЛОГИН ЕСТЬ");
-        Alert alert = new Alert(Alert.AlertType.WARNING);
+    public void mainButtonClick() throws IOException {
         //alert.initOwner(root.getPrimaryStage );
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/main.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("ЧАТ");
+        stage.show();
     }
 
 }
