2fef4054f099fe2cee6ed16767e8f3b40b63e44f
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-02-21 22:55:27 +0300

    Кароч тут будет общение с API, а я спать

---
+++ client/src/main/java/sample/APIRequest.java
@@ -0,0 +1,21 @@
+package sample;
+
+
+
+public class APIRequest {
+
+    String ServerURL = "http://127.0.0.1";
+
+    /*
+    Регистрация пользователя
+     */
+    public static void Registration(String name, String login, String password){
+        r
+
+    }
+
+    public APIRequest(String login, String password ){
+
+
+    }
+}
