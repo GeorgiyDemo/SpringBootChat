0b26051274e3d74321ad76b8abdc556568106857
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-04-08 19:42:18 +0300

    UI регистрации есть #5

--- newclient/newclient/src/main/java/org/demka/controllers/RegistrationController.java
+++ newclient/newclient/src/main/java/org/demka/controllers/RegistrationController.java
@@ -1,5 +1,8 @@
 package org.demka.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
 import org.demka.App;
 
 public class RegistrationController extends SuperFullController {
@@ -8,6 +11,19 @@ public class RegistrationController extends SuperFullController {
      * Метод инициализации (вызывается с Main)
      * @param mainApp
      */
+
+    @FXML
+    private JFXButton BackButton;
+    @FXML
+    private JFXButton RegButton;
+
+    @FXML
+    private JFXTextField UserNameTextField;
+    @FXML
+    private JFXTextField EmailTextField;
+    @FXML
+    private JFXTextField PasswordTextField;
+
     @Override
     public void initialize(App mainApp) {
         this.mainApp = mainApp;
