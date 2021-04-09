3a0efaf9a84b9880927e91adad3118fae55729e2
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-04-09 21:37:40 +0300

    Расположили интерфейсы для смены пароля #17
    И я устал

--- newclient/newclient/src/main/java/org/demka/controllers/ForgotPasswordController.java
+++ newclient/newclient/src/main/java/org/demka/controllers/ForgotPasswordController.java
@@ -1,17 +1,31 @@
 package org.demka.controllers;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import org.demka.App;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ForgotPasswordController extends SuperFullController {
+
+    @FXML
+    JFXPasswordField NewPasswordTextField;
+
+    @FXML
+    JFXPasswordField MasterKeyTextField;
+
+    @FXML
+    JFXTextField LoginTextField;
+
+    private static final Logger logger = LoggerFactory.getLogger(ForgotPasswordController.class);
+
+
     /**
      * Метод инициализации (вызывается с Main)
      *
      * @param mainApp
      */
-    private static final Logger logger = LoggerFactory.getLogger(ForgotPasswordController.class);
     @Override
     public void initialize(App mainApp) {
         this.mainApp = mainApp;
@@ -21,5 +35,10 @@ public class ForgotPasswordController extends SuperFullController {
     private void backButtonClicked(){
         mainApp.UserAuthorisation();
         logger.info("Выход из формы смены пароля");
+    }
+
+    @FXML
+    private void resetButtonClicked(){
+        logger.info("Нажатие на button сброса пароля");
     }
 }
